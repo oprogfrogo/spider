7951f18d58449365b0e877b37b63001a6c1ed2b0
Simon Lee <oprogfrogo@gmail.com>
2017-07-31 22:26:36 -0700

    modify schema file

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20170614062020) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
+  
+  create_table "users", force: :cascade do |t|
+    t.string   "email",      limit: 255
+    t.string   "password",   limit: 255
+    t.string   "token",      limit: 255
+    t.string   "confirmed",  limit: 255, default: "N"
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+  end
 
   create_table "autos", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
@@ -159,15 +168,6 @@ ActiveRecord::Schema.define(version: 20170614062020) do
     t.string   "password",   limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",      limit: 255
-    t.string   "password",   limit: 255
-    t.string   "token",      limit: 255
-    t.string   "confirmed",  limit: 255, default: "N"
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
   end
 
   add_foreign_key "profiles", "users"
